ee7ceaa867728acbc2907856c971dddad678efe8
annavlin <Anechka211299@yandex.ru>
2026-05-01 02:33:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,16 @@
   textColor: '#000000',
   height: height: window.innerHeight,
   fullWidth: true,
+  (function (t, p) {
+  window.Marquiz
+    ? Marquiz.add([t, p])
+    : document.addEventListener('marquizLoaded', function () {
+        Marquiz.add([t, p]);
+      });
+})('Inline', {
+  id: '69ef2ea44d59b50019b6b511',
+  height: window.innerHeight,
+  fullWidth: true
 });
+});
+
